34520610c49cd90c291d1e9e628cba0649e26a50
antdimarino <37364119+antdimarino@users.noreply.github.com>
2019-03-13 15:50:39 +0100

    Update wrapper.h
    Edit: FullRead

--- wrapper.h
+++ wrapper.h
@@ -10,6 +10,7 @@
 #include<pthread.h>
 #include<semaphore.h>
 #include<fcntl.h>
+#include<signal.h>
 
 
 ssize_t FullWrite(int fd, const void *buf, size_t count);
@@ -70,6 +71,7 @@ ssize_t FullRead(int fd, void *buf, size_t count)
                 exit(nread);
             }
         }else if (nread==0){
+            nleft = -1;
             break;
         }
         nleft-=nread;
